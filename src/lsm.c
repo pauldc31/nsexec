7832f7761053c70dbe64d4038c924f9169d62320
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-02-28 06:23:50 -0300

    lsm: Return true if no context is informed
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- src/lsm.c
+++ src/lsm.c
@@ -5,8 +5,6 @@
 
 #ifdef HAVE_SELINUX
 #include <selinux/selinux.h>
-
-#define DEFAULT_CONTEXT "unconfined_u:unconfined_r:unconfined_t:s0"
 #endif
 
 bool set_context(char *context)
@@ -14,10 +12,9 @@ bool set_context(char *context)
 	(void)context;
 #ifdef HAVE_SELINUX
 	if (!context)
-		context = DEFAULT_CONTEXT;
+		return true;
 
-	verbose("Using SELinux context: %s\n", context);
-
+	verbose("Setting SELinux context: %s\n", context);
 	if (setexeccon_raw(context) < 0) {
 		perror("setexeccon");
 		return false;
