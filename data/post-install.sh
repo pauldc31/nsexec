eac66f2710c107be6008fe887c276157d4db6894
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-05-31 08:34:48 -0300

    post-install.sh: Make post-install DESTDIR aware
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- data/post-install.sh
+++ data/post-install.sh
@@ -2,5 +2,5 @@
 
 # just add the setcap if user is root
 if [ "$UID" = "0" ]; then
-	setcap cap_net_admin+ep "$MESON_INSTALL_PREFIX"/bin/nsexec_nic
+	setcap cap_net_admin+ep "$DESTDIR$MESON_INSTALL_PREFIX"/bin/nsexec_nic
 fi
